eb0dcf007bb6b469490bac7aad46937bcc82dd35
Amit <amitkr24031995@gmail.com>
2021-10-07 18:02:24 +0530

    Adding UC1createStack

---
+++ UC1createStack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UC1createStack
+{
+   
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            LinkedListStack myNum = new LinkedListStack();
+            myNum.Push(70);
+            myNum.Push(30);
+            myNum.Push(56);
+            myNum.Display();
+        }
+    }
+}
